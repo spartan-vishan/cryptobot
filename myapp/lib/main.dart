5d8bc09d7c5e38e317a3b8c4e5f880a594591821
Ibrahim Ansari <ibrahim2221916@gmail.com>
2018-05-09 00:56:20 -0700

    Readability of Code part 2

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,16 +1,16 @@
 import 'dart:async';
-import 'dart:math';
 import 'dart:io';
+import 'dart:math';
 
-import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:image_picker/image_picker.dart';
 
 // There Date for iOS
@@ -43,9 +43,8 @@ class RealTimeChatApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: "RealTimeChat",
-      theme: defaultTargetPlatform == TargetPlatform.iOS
-          ? iOSTheme
-          : androidTheme,
+      theme:
+          defaultTargetPlatform == TargetPlatform.iOS ? iOSTheme : androidTheme,
       home: new LoginPage(),
     );
   }
@@ -57,9 +56,10 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("RealTimeChat", style: new TextStyle(fontStyle: FontStyle.italic, color: Colors.white)),
+        title: new Text("RealTimeChat",
+            style: new TextStyle(
+                fontStyle: FontStyle.italic, color: Colors.white)),
         elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
-
       ),
       body: new Center(
         child: new RaisedButton(
@@ -85,15 +85,14 @@ class LoginPage extends StatelessWidget {
 // Tries to Login silently if User has used app before, else forces Google login
 Future<Null> _ensureLoggedIn() async {
   GoogleSignInAccount user = googleSignIn.currentUser;
-  if (user == null)
-    user = await googleSignIn.signInSilently();
+  if (user == null) user = await googleSignIn.signInSilently();
   if (user == null) {
     user = await googleSignIn.signIn();
     analytics.logLogin();
   }
   if (await auth.currentUser() == null) {
     GoogleSignInAuthentication credentials =
-    await googleSignIn.currentUser.authentication;
+        await googleSignIn.currentUser.authentication;
     await auth.signInWithGoogle(
       idToken: credentials.idToken,
       accessToken: credentials.accessToken,
@@ -105,13 +104,13 @@ Future<Null> _ensureLoggedIn() async {
 @override
 class MessageWidget extends StatelessWidget {
   MessageWidget({this.snapshot, this.animation});
+
   final DataSnapshot snapshot;
   final Animation animation;
 
   Widget build(BuildContext context) {
     return new SizeTransition(
-      sizeFactor: new CurvedAnimation(
-          parent: animation, curve: Curves.easeOut),
+      sizeFactor: new CurvedAnimation(parent: animation, curve: Curves.easeOut),
       axisAlignment: 0.0,
       child: new Container(
         margin: const EdgeInsets.symmetric(vertical: 10.0),
@@ -120,23 +119,24 @@ class MessageWidget extends StatelessWidget {
           children: <Widget>[
             new Container(
               margin: const EdgeInsets.only(right: 16.0),
-              child: new CircleAvatar(backgroundImage: new NetworkImage(snapshot.value['senderPhotoUrl'])),
+              child: new CircleAvatar(
+                  backgroundImage:
+                      new NetworkImage(snapshot.value['senderPhotoUrl'])),
             ),
             new Expanded(
               child: new Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  new Text(
-                      snapshot.value['senderName'],
+                  new Text(snapshot.value['senderName'],
                       style: Theme.of(context).textTheme.subhead),
                   new Container(
                     margin: const EdgeInsets.only(top: 5.0),
-                    child: snapshot.value['imageUrl'] != null ?
-                    new Image.network(
-                      snapshot.value['imageUrl'],
-                      width: 250.0,
-                    ) :
-                    new Text(snapshot.value['text']),
+                    child: snapshot.value['imageUrl'] != null
+                        ? new Image.network(
+                            snapshot.value['imageUrl'],
+                            width: 250.0,
+                          )
+                        : new Text(snapshot.value['text']),
                   ),
                 ],
               ),
@@ -164,12 +164,12 @@ class ChatWidgetState extends State<ChatWidget> {
   Widget build(BuildContext context) {
     return new Scaffold(
         appBar: new AppBar(
-          title: new Text("RealTimeChat",
-                  style: new TextStyle(fontStyle: FontStyle.italic,
-                  color: Colors.white)),
-          elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
-            automaticallyImplyLeading: false
-        ),
+            title: new Text("RealTimeChat",
+                style: new TextStyle(
+                    fontStyle: FontStyle.italic, color: Colors.white)),
+            elevation:
+                Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+            automaticallyImplyLeading: false),
         body: new Column(children: <Widget>[
           new Flexible(
             child: new FirebaseAnimatedList(
@@ -177,18 +177,16 @@ class ChatWidgetState extends State<ChatWidget> {
               sort: (a, b) => b.key.compareTo(a.key),
               padding: new EdgeInsets.all(8.0),
               reverse: true,
-              itemBuilder: (_, DataSnapshot snapshot, Animation<double> animation) {
+              itemBuilder:
+                  (_, DataSnapshot snapshot, Animation<double> animation) {
                 return new MessageWidget(
-                    snapshot: snapshot,
-                    animation: animation
-                );
+                    snapshot: snapshot, animation: animation);
               },
             ),
           ),
           new Divider(height: 1.0),
           new Container(
-            decoration:
-            new BoxDecoration(color: Theme.of(context).cardColor),
+            decoration: new BoxDecoration(color: Theme.of(context).cardColor),
             child: _buildTextComposer(),
           ),
         ]));
@@ -208,13 +206,13 @@ class ChatWidgetState extends State<ChatWidget> {
                     await _ensureLoggedIn();
                     File imageFile = await ImagePicker.pickImage();
                     int random = new Random().nextInt(100000);
-                    StorageReference ref =
-                    FirebaseStorage.instance.ref().child("image_$random.jpg");
+                    StorageReference ref = FirebaseStorage.instance
+                        .ref()
+                        .child("image_$random.jpg");
                     StorageUploadTask uploadTask = ref.put(imageFile);
                     Uri downloadUrl = (await uploadTask.future).downloadUrl;
                     _sendMessage(imageUrl: downloadUrl.toString());
-                  }
-              ),
+                  }),
             ),
             new Flexible(
               child: new TextField(
@@ -226,29 +224,29 @@ class ChatWidgetState extends State<ChatWidget> {
                 },
                 onSubmitted: _handleSubmitted,
                 decoration:
-                new InputDecoration.collapsed(hintText: "Send a message"),
+                    new InputDecoration.collapsed(hintText: "Send a message"),
               ),
             ),
             new Container(
                 margin: new EdgeInsets.symmetric(horizontal: 4.0),
                 child: Theme.of(context).platform == TargetPlatform.iOS
                     ? new CupertinoButton(
-                  child: new Text("Send"),
-                  onPressed: _isComposing
-                      ? () => _handleSubmitted(_textController.text)
-                      : null,
-                )
+                        child: new Text("Send"),
+                        onPressed: _isComposing
+                            ? () => _handleSubmitted(_textController.text)
+                            : null,
+                      )
                     : new IconButton(
-                  icon: new Icon(Icons.send),
-                  onPressed: _isComposing
-                      ? () => _handleSubmitted(_textController.text)
-                      : null,
-                )),
+                        icon: new Icon(Icons.send),
+                        onPressed: _isComposing
+                            ? () => _handleSubmitted(_textController.text)
+                            : null,
+                      )),
           ]),
           decoration: Theme.of(context).platform == TargetPlatform.iOS
               ? new BoxDecoration(
-              border:
-              new Border(top: new BorderSide(color: Colors.grey[200])))
+                  border:
+                      new Border(top: new BorderSide(color: Colors.grey[200])))
               : null),
     );
   }
@@ -264,7 +262,7 @@ class ChatWidgetState extends State<ChatWidget> {
   }
 
   // Pushes message to Firebase
-  void _sendMessage({ String text, String imageUrl }) {
+  void _sendMessage({String text, String imageUrl}) {
     reference.push().set({
       'text': text,
       'imageUrl': imageUrl,
@@ -273,5 +271,4 @@ class ChatWidgetState extends State<ChatWidget> {
     });
     analytics.logEvent(name: 'send_message');
   }
-
 }
